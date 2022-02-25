27a57cdba4669175a7053d8c486af281671887de
meziane <meziane.amrouche@labom2iformation.fr>
2022-02-25 14:38:26 +0100

    ajout servicesHistoriques

--- Intranet/TpIntranetEntreprise/SQL/CreateTableCollab.sql
+++ Intranet/TpIntranetEntreprise/SQL/CreateTableCollab.sql
@@ -1,9 +1,13 @@
-﻿CREATE TABLE typeCollaborateurs(
+﻿Drop table typeCollaborateurs;
+Drop table services;
+Drop table collaborateurs;
+
+CREATE TABLE typeCollaborateurs(
 idTypeCollab int PRIMARY KEY IDENTITY(1,1),
 nomTypeCollab varchar(100) not null
 );
 
-CREATE TABLE services(
+CREATE TABLE servicesCollab(
 idService int PRIMARY KEY IDENTITY(1,1),
 nomService varchar(100) not null
 );
@@ -14,6 +18,13 @@ nom varchar(100) not null,
 prenom varchar(100) not null,
 dateNaissance datetime not null,
 mdp varchar(20) not null,
-idTypeCollab int FOREIGN KEY REFERENCES typeCollaborateurs(idTypeCollab),
-idService int FOREIGN KEY REFERENCES services(idService)
+idTypeCollab int FOREIGN KEY REFERENCES typeCollaborateurs(idTypeCollab)
+);
+
+CREATE TABLE servicesHistoriques(
+idServiceHistorique int PRIMARY KEY IDENTITY(1,1),
+dateDebut datetime not null,
+dateFin datetime null,
+matricule int FOREIGN KEY REFERENCES collaborateurs(matriculeCollab),
+idService int FOREIGN KEY REFERENCES servicesCollab(idService)
 );
