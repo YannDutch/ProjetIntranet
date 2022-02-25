38a988f28806e82c7ce78ee25a0e00bf07582f11
meziane <meziane.amrouche@labom2iformation.fr>
2022-02-25 14:07:55 +0100

    create table collab

---
+++ Intranet/TpIntranetEntreprise/SQL/CreateTableCollab.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE typeCollaborateurs(
+idTypeCollab int PRIMARY KEY IDENTITY(1,1),
+nomTypeCollab varchar(100) not null
+);
+
+CREATE TABLE services(
+idService int PRIMARY KEY IDENTITY(1,1),
+nomService varchar(100) not null
+);
+
+CREATE TABLE collaborateurs(
+matriculeCollab int PRIMARY KEY IDENTITY(1,1),
+nom varchar(100) not null,
+prenom varchar(100) not null,
+dateNaissance datetime not null,
+mdp varchar(20) not null,
+idTypeCollab int FOREIGN KEY REFERENCES typeCollaborateurs(idTypeCollab),
+idService int FOREIGN KEY REFERENCES services(idService)
+);
